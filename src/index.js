86fb143911299fc5b50380f116e3c69b66c676fc
GrimdvlDesktop <grimdvl@gmail.com>
2024-04-23 08:16:58 +0300

    1st commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// const elem = <h2>Hello World!</h2>;
+// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!');
+// const element = {
+//   type: 'h2',
+//   props: {
+//     className: 'greeting',
+//     children: 'Hello World!'
+//   }
+// };
+const text = 'Hello World!';
+
+const elem = (
+	<div>
+		<h2>{text}</h2>
+		<input type="text" />
+		<button></button>
+	</div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	elem,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
