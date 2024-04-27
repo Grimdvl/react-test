978eb63dd06e4390e03fe36b4a980fd93a221b7a
GrimdvlLaptop <grimdvl@gmail.com>
2024-04-27 12:43:43 +0300

    10 lesson done

--- src/App.js
+++ src/App.js
@@ -36,10 +36,22 @@ function Btn() {
 	return <button>{logged ? 'Enter' : res()}</button>;
 }
 
+function WhoAmI ({name, surname, link}) {
+	return (
+		<div>
+			<h1>My name is {name}, surname - {surname.firstSurname}</h1>
+			<a href={link()}>My profile</a>
+		</div>
+	)
+}
+
 function App() {
 	return (
 		<div className="App">
 			<Header/>
+			<WhoAmI name="Jhon" surname={{firstSurname: 'Smith'}} link={() => {return "facebook.com"}}/>
+			<WhoAmI name='Ivan' surname={{firstSurname: 'Smith'}} link={() => {return "facebook.com"}}/>
+			<WhoAmI name="Alex" surname={{firstSurname: 'Smith'}} link={() => {return "facebook.com"}}/>
 			<Field/>
 			<Btn/>
 		</div>
