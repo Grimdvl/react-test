6bc50969189b3af27493f6ef970d7eba876c600a
GrimdvlLaptop <grimdvl@gmail.com>
2024-04-28 14:10:38 +0300

    lesson 13 done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 const Header = () => {
@@ -36,13 +36,39 @@ function Btn() {
 	return <button>{logged ? 'Enter' : res()}</button>;
 }
 
-function WhoAmI ({name, surname, link}) {
-	return (
-		<div>
-			<h1>My name is {name}, surname - {surname.firstSurname}</h1>
-			<a href={link()}>My profile</a>
-		</div>
-	)
+// function WhoAmI ({name, surname, link}) {
+// 	return (
+// 		<div>
+// 			<h1>My name is {name}, surname - {surname.firstSurname}</h1>
+// 			<a href={link()}>My profile</a>
+// 		</div>
+// 	)
+// }
+
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 27,
+			text: '+++',
+		}
+	}
+	nextYear = () => {
+		console.log('+++');
+		this.setState(state => ({
+			years: state.years + 1
+		}));
+	}
+	render() {
+		const { name, surname, link } = this.props;
+		return (
+			<div>
+				<button onClick={this.nextYear}>{this.state.text}</button>
+				<h1>My name is {name}, surname - {surname.firstSurname}, age - {this.state.years}</h1>
+				<a href={link()}>My profile</a>
+			</div>
+		)
+	}
 }
 
 function App() {
