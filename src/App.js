3a60e1e9937e5130d43c43863cbddc2f49fdc595
GrimdvlLaptop <grimdvl@gmail.com>
2024-11-07 08:45:02 +0200

    22 lesson almost done 17 00

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import BootstrapTest from './BootstrapTest';
 import {Container} from 'react-bootstrap';
 import ReactDOM from 'react-dom';
 import { CSSTransition } from 'react-transition-group';
+import Form from './Form';
 
-import './App.css';
+// import './App.css';
+import './app.scss';
 
 // const EmpItem = styled.div`
 // 	padding: 20px;
@@ -332,77 +334,85 @@ import './App.css';
 //     );
 // }
 
-const Modal = (props) => {
+// const Modal = (props) => {
 
-	const duration = 300;
-	const nodeRef = useRef(null);
+// 	const duration = 300;
+// 	const nodeRef = useRef(null);
 
-	// const defaultStyle = {
-	// 	transition: `all ${duration}ms ease-in-out`,
-	// 	opacity: 0,
-	// 	visibility: 'hidden'
-	// }
+// 	// const defaultStyle = {
+// 	// 	transition: `all ${duration}ms ease-in-out`,
+// 	// 	opacity: 0,
+// 	// 	visibility: 'hidden'
+// 	// }
 
-	// const transitionStyles = {
-	// 	entering: { opacity: 1, visibility: 'visible' },
-	// 	entered: { opacity: 1, visibility: 'visible' },
-	// 	exiting: { opacity: 0, visibility: 'hidden' },
-	// 	exited: { opacity: 0, visibility: 'hidden' },
-	// }
+// 	// const transitionStyles = {
+// 	// 	entering: { opacity: 1, visibility: 'visible' },
+// 	// 	entered: { opacity: 1, visibility: 'visible' },
+// 	// 	exiting: { opacity: 0, visibility: 'hidden' },
+// 	// 	exited: { opacity: 0, visibility: 'hidden' },
+// 	// }
 
-    return (
-		<CSSTransition
-			in={props.show}
-			timeout={duration}
-			unmountOnExit
-			unmountOnEnter
-			onEnter={() => props.setShowTrigger(false)}
-			onExited={() => props.setShowTrigger(true)}
-			nodeRef={nodeRef}
-			classNames="modal">
-			{/* {state => ( */}
-				<div className="modal mt-5 d-block"
-					// style={{
-					// ...defaultStyle,
-					// ...transitionStyles[state]}}
-					ref={nodeRef}>
-					<div className="modal-dialog">
-						<div className="modal-content">
-							<div className="modal-header">
-								<h5 className="modal-title">Typical modal window</h5>
-								<button onClick={() => props.onClose(false)} type="button" className="btn-close" aria-label="Close"></button>
-							</div>
-							<div className="modal-body">
-								<p>Modal body content</p>
-							</div>
-							<div className="modal-footer">
-								<button onClick={() => props.onClose(false)} type="button" className="btn btn-secondary">Close</button>
-								<button onClick={() => props.onClose(false)} type="button" className="btn btn-primary">Save changes</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			{/* )} */}
-		</CSSTransition>
-    )
-}
+//     return (
+// 		<CSSTransition
+// 			in={props.show}
+// 			timeout={duration}
+// 			unmountOnExit
+// 			unmountOnEnter
+// 			onEnter={() => props.setShowTrigger(false)}
+// 			onExited={() => props.setShowTrigger(true)}
+// 			nodeRef={nodeRef}
+// 			classNames="modal">
+// 			{/* {state => ( */}
+// 				<div className="modal mt-5 d-block"
+// 					// style={{
+// 					// ...defaultStyle,
+// 					// ...transitionStyles[state]}}
+// 					ref={nodeRef}>
+// 					<div className="modal-dialog">
+// 						<div className="modal-content">
+// 							<div className="modal-header">
+// 								<h5 className="modal-title">Typical modal window</h5>
+// 								<button onClick={() => props.onClose(false)} type="button" className="btn-close" aria-label="Close"></button>
+// 							</div>
+// 							<div className="modal-body">
+// 								<p>Modal body content</p>
+// 							</div>
+// 							<div className="modal-footer">
+// 								<button onClick={() => props.onClose(false)} type="button" className="btn btn-secondary">Close</button>
+// 								<button onClick={() => props.onClose(false)} type="button" className="btn btn-primary">Save changes</button>
+// 							</div>
+// 						</div>
+// 					</div>
+// 				</div>
+// 			{/* )} */}
+// 		</CSSTransition>
+//     )
+// }
+
+// function App() {
+//     const [showModal, setShowModal] = useState(false);
+// 	const [showTrigger, setShowTrigger] = useState(true);
+
+//     return (
+//         <Container>
+// 			<Modal show={showModal} onClose={setShowModal} setShowTrigger={setShowTrigger}/>
+//             {/* {showModal ? <Modal onClose={setShowModal}/> : null} */}
+// 			{showTrigger ? 
+// 				<button 
+// 				type="button" 
+// 				className="btn btn-warning mt-5"
+// 				onClick={() => setShowModal(true)}>Open Modal</button> :
+// 				null}
+//         </Container>
+//     );
+// }
 
 function App() {
-    const [showModal, setShowModal] = useState(false);
-	const [showTrigger, setShowTrigger] = useState(true);
-
-    return (
-        <Container>
-			<Modal show={showModal} onClose={setShowModal} setShowTrigger={setShowTrigger}/>
-            {/* {showModal ? <Modal onClose={setShowModal}/> : null} */}
-			{showTrigger ? 
-				<button 
-				type="button" 
-				className="btn btn-warning mt-5"
-				onClick={() => setShowModal(true)}>Open Modal</button> :
-				null}
-        </Container>
-    );
+	return (
+		<div className='app'>
+			<Form/>
+		</div>
+	)
 }
 
 // export {Header};
